f85c8b73a3f55d49af6c6a647e65974f32db8faa
rhystus <rhystus@gmail.com>
2019-06-24 21:01:58 -0400

    added new dialog for adding users. closes #8

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -2,6 +2,9 @@ import { Component, OnInit } from '@angular/core';
 import { DataService } from '../data.service';
 
 import { User } from '../models/user';
+import { AddDialogComponent } from '../add-dialog/add-dialog.component';
+import { MatDialog } from '@angular/material';
+import { FormGroup, Validators, FormBuilder } from '@angular/forms';
 
 @Component({
   selector: 'app-home',
@@ -11,8 +14,17 @@ import { User } from '../models/user';
 export class HomeComponent implements OnInit {
 
   h1Style = false;
+  addDialogForm: FormGroup;
 
-  constructor(private data: DataService) { }
+  constructor(
+    private data: DataService, 
+    public dialog: MatDialog,
+    private formBuilder: FormBuilder
+    ) { 
+      this.addDialogForm = this.formBuilder.group({
+        name: ['', Validators.required]
+      });
+    }
 
   users: User[] = [];
   // users: Array<User>;
@@ -28,5 +40,19 @@ export class HomeComponent implements OnInit {
   onSeatClick(i: number): void {
     this.users.splice(i, 1);
   }
-}
 
+  openAddDialog(): void {
+    this.addDialogForm.setValue({name: ''});
+
+    const dialogRef = this.dialog.open(AddDialogComponent, {
+      width: '500px',
+      data: this.addDialogForm
+    });
+
+    dialogRef.afterClosed().subscribe((result: boolean) => {
+      if (result) {
+        DataService.addUser({name: this.addDialogForm.controls.name.value});
+      }
+    });
+  }
+}
